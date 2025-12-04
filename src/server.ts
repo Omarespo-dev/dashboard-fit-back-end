4773064601dd634d5c4c11434f5cf36f08e9c052
Omarespo-dev <omaresposito21@gmail.com>
2025-12-04 14:31:16 +0100

    added middleware Global

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,21 @@
 //Importiamo express e i suoi tipi req e res
 import express, { Request, Response } from "express";
-//Importo dotenv per caricare le variabili dal file .env
 import dotenv from "dotenv";
+import cors from "cors";
+import helmet from "helmet";
 
 //usiamo express
 const app = express();
 
 // Carica le variabili dal file .env in process.env
 dotenv.config();
-
 //Ricavo la porta
 const port = process.env.PORT;
+
+//Middleware Globali
+app.use(express.json()); // legge Json da req
+app.use(cors({ origin: "http://localhost:5173" }));
+app.use(helmet()); // helmet per headers di sicurezza
 
 //Definiamo la rotta home, gestendo la richiesta del client e la risposta
 app.get("/", (req: Request, res: Response) => {
